625e6b26aabe4168a107037f1e240fcae9dfc881
ArinaRiter <117931793+ArinaRiter@users.noreply.github.com>
2023-10-05 11:35:06 +0300

    menu again

---
+++ Assets/Scripts/ArButton.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using TMPro;
+using UnityEngine;
+
+
+
+    public class ArButton : MonoBehaviour
+    {
+        public event Action OnButtonClicked;
+
+        [SerializeField] private TextMeshPro _title;
+
+        [SerializeField] private Interactable _interactable;
+
+        private GameObject _prefab;
+
+        public void Initialize(Item config)
+        {
+            _title.text = config.Title;
+            _prefab = config.Prefab;
+
+            _interactable.OnClick.AddListener(ProccessClick);
+        }
+
+        private void ProccessClick()
+        {
+            Instantiate(_prefab);
+            OnButtonClicked?.Invoke();
+        }
+    }
+
